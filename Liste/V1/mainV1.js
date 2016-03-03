ece538d2b80ee58ec129498a11f54adaf8794ad2
AlexD <alxdmr2@yahoo.fr>
2016-03-03 15:11:00 +0100

    Angular

---
+++ Liste/V1/mainV1.js
@@ -0,0 +1,25 @@
+// Framework Angular
+var angular 		= require( "angular" 			)
+  , angularMaterial	= require( "angular-material" 	)
+  , angularAria 	= require( "angular-aria" 		)
+  , angularAnimate	= require( "angular-animate" 	)
+  ;
+require( "angular-material/angular-material.css" );
+
+
+// Instantiate module
+var mod = angular.module( "bshm", ["ngMaterial"] );
+
+// Define a directive to deal with data
+var ListeChoses = require( "../NF/ListeChoses.js" );
+mod.directive( "bshmRoot", function() {return {
+	bindToController: true,
+	controllerAs	: "ctrl",
+	controller 		: function() {
+		this.hello = "hello";
+		this.listeChoses = new ListeChoses();
+	}
+};})
+
+// Load components
+require( "./js/ListeChoses.js" )(mod);
